e135631c6752066f3fb608f7b96d25fbfc60cb59
KDani-99 <porsekin@gmail.com>
2020-12-13 23:18:14 +0100

    test: request handling

--- DeploymentHelper.cpp
+++ DeploymentHelper.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include "config/Config.hpp"
+
 #include <chrono>
+#include "config/Config.hpp"
+#include "Server.hpp"
+
+#include "Security.hpp"
+
 int main()
 {
-	/* Optimized for Ubuntu/Windows */
 
 	Config config = Config();
 	auto t1 = std::chrono::high_resolution_clock::now();
@@ -12,5 +16,12 @@ int main()
 
 	std::cout << ::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << std::endl;
 
+	string_t address = U("http://127.0.0.1:1011");
+	HTTPServer server(address,config);
+
+	std::string line;
+	std::getline(std::cin, line);
+
     std::cout << "Hello World!\n";
+	return 0;
 }
