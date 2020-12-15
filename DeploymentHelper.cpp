4affa84fff21617f686ff7a56ff3d412c0b7aabb
KDani-99 <porsekin@gmail.com>
2020-12-15 11:41:32 +0100

    refactor: only work with required classes

--- DeploymentHelper.cpp
+++ DeploymentHelper.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include <chrono>
 #include "src/Config/Config.hpp"
 #include "src/HTTP/Server/Server.hpp"
@@ -9,11 +8,7 @@ int main()
 	// Designed for reverse proxys, no ssl support (yet)
 
 	Config config = Config();
-	auto t1 = std::chrono::high_resolution_clock::now();
 	config.LoadConfig();
-	auto t2 = std::chrono::high_resolution_clock::now();
-
-	std::cout << ::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << std::endl;
 
 	string_t address = U("http://127.0.0.1:1011");
 	HTTPServer server(config);
