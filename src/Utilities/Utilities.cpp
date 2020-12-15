c22e214f3690678422fcad81f213f7462f96292f
KDani-99 <porsekin@gmail.com>
2020-12-15 11:40:09 +0100

    feat: uuid generator using boost

--- src/Utilities/Utilities.cpp
+++ src/Utilities/Utilities.cpp
@@ -18,3 +18,12 @@ string_t Utilities::ToString_T(unsigned short input)
 {
 	return ToString_T(std::to_string(input));
 }
+std::string Utilities::GenerateUUID()
+{
+	boost::uuids::uuid uuid = boost::uuids::random_generator()();
+	return boost::lexical_cast<std::string>(uuid);
+}
+int Utilities::GetCurrentTimestamp()
+{
+	return time(NULL);
+}
--- src/Utilities/Utilities.hpp
+++ src/Utilities/Utilities.hpp
@@ -1,7 +1,13 @@
 #pragma once
 #include <string>
 #include <stdlib.h>
+#include <ctime>
 #include <cpprest/containerstream.h>    
+
+#include <boost/uuid/uuid.hpp> 
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/lexical_cast.hpp>
 
 using namespace utility;
 
@@ -11,4 +17,6 @@ class Utilities
 		static std::string GetProjectName(std::string fullPath);
 		static string_t ToString_T(std::string input);
 		static string_t ToString_T(unsigned short input);
+		static std::string GenerateUUID();
+		static int GetCurrentTimestamp();
 };
