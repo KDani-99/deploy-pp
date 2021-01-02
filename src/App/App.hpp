bd6b4079d0a00aa8767ba54ec24d409f2f9375a1
KDani-99 <porsekin@gmail.com>
2021-01-03 00:53:27 +0100

    refactor: logger ptr

--- src/App/App.hpp
+++ src/App/App.hpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include "../Logger/Logger.hpp"
 #include "../Step/Step.hpp"
 
 class App
@@ -18,12 +19,16 @@ class App
 		std::string m_webhookSecret;
 		bool m_force;
 		std::vector<Step> m_steps;
+
+		Logger* m_logger;
+
 	public:
-		App(std::string name, std::string repository, std::string local,std::string branch,std::string m_webhookSecret,bool force, std::vector<Step> steps);
+		App(std::string name, std::string repository, std::string local,std::string branch,std::string m_webhookSecret,bool force, std::vector<Step> steps,Logger * logger);
 		std::string GetName();
 		std::string GetRemoteRepository();
 		std::string GetLocalRepositoryPath();
 		std::string GetWebhookSecret();
+		std::vector<Step> GetSteps(); // [deprecated]
 
 		void Pull();
 		void TriggerActions();
