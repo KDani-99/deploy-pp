6ce52c6d3b4705a13ad59b70af6caa03e98f0d40
KDani-99 <porsekin@gmail.com>
2021-01-03 00:54:21 +0100

    feat: success logging

--- src/Logger/Logger.cpp
+++ src/Logger/Logger.cpp
@@ -55,3 +55,45 @@ void Logger::Error(std::string label, std::string info)
 {
 	this->Log("ERROR", label, info);
 }
+void Logger::Success(std::string delivery, std::string event, int hookId, std::string before, std::string after)
+{
+	std::string date = Logger::GetDate();
+	try
+	{
+		YAML::Emitter yamlLog;
+		yamlLog << YAML::BeginMap;
+		yamlLog << YAML::Key << "X-GitHub-Delivery";
+		yamlLog << YAML::Value << delivery;
+		yamlLog << YAML::Key << "X-GitHub-Event";
+		yamlLog << YAML::Value << event;
+		yamlLog << YAML::Key << "X-GitHub-Hook-ID";
+		yamlLog << YAML::Value << hookId;
+		yamlLog << YAML::Key << "Before";
+		yamlLog << YAML::Value << before;
+		yamlLog << YAML::Key << "After";
+		yamlLog << YAML::Value << after;
+		yamlLog << YAML::Key << "Timestamp";
+		yamlLog << YAML::Value << date;
+		yamlLog << YAML::EndMap;
+
+		std::cout << yamlLog.c_str() << std::endl;
+
+		std::string fileName = std::to_string(hookId);
+		fileName += ".yaml";
+
+		if (!boost::filesystem::exists("success_logs"))
+			boost::filesystem::create_directory("success_logs");
+
+		std::ofstream log = std::ofstream("success_logs/"+fileName, std::ofstream::app);
+		
+		log << yamlLog.c_str();
+
+		log.close();
+
+	}
+	catch (const std::exception& ex)
+	{
+		std::string failed = date + " [ ERROR ] [ LOGGER ]: Failed to save success log. Exception: " + ex.what() + "\n";
+		printf(failed.c_str());
+	}
+}
